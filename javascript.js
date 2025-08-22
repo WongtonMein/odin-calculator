84b29e778f9243812096cbd0ac0b3b97f764be8f
WongtonMein <190700700+WongtonMein@users.noreply.github.com>
2025-08-22 16:59:35 -0700

    Built UI

--- javascript.js
+++ javascript.js
@@ -1,27 +1,46 @@
-function add(a, b) {
-    console.log( a + b )
-}
+function add(a, b) { return a + b }
 
-function subtract(a, b) {
-    console.log( a - b )
-}
+function subtract(a, b) { return a - b }
 
-function multiply(a, b) {
-    console.log( a * b )
-}
+function multiply(a, b) { return a * b }
 
-function divide(a, b) {
-    console.log( a / b )
-}
+function divide(a, b) { return a / b }
 
 function operate(a, b, operator) {
     if (operator === "+") {
-        add(a, b);
+        return add(a, b);
     } else if (operator === "-") {
-        subtract(a, b);
+        return subtract(a, b);
     } else if (operator === "*") {
-        multiply(a, b);
+        return multiply(a, b);
     } else if (operate === "/") {
-        divide(a, b);
+        return divide(a, b);
     }
 };
+
+let a;
+let b;
+let operator;
+
+let display = document.getElementById("results");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+// const clear = document.getElementById("clear");
+// const parentheses = document.getElementById("parentheses");
+// const delete = document.getElementById("delete");
+// const parentheses = document.getElementById("parentheses");
+// const decimal = document.getElementById("decimal");
+const enter = document.getElementById("enter");
+
