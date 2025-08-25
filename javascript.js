b382e5974f4c5a691ad1a141ed6384efe6a4a145
WongtonMein <190700700+WongtonMein@users.noreply.github.com>
2025-08-25 15:34:21 -0700

    Finished basic calculator functionality

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ let firstValue = null;
 let secondValue = null;
 let operator = null;
 let result = null;
+let userInput = [];
+let calculated = false;
+const roundedDecimals = 4;
 // #endregion
 
 // #region --- button inputs
@@ -15,14 +18,22 @@ const equalBtn = document.getElementById("enter");
 const decimalBtn = document.getElementById("decimal");
 
 // #region --- basic math funcs
-function addFunc(a, b) { return a + b };
-function subtractFunc(a, b) { return a - b };
-function multiplyFunc(a, b) { return a * b };
+function addFunc(a, b) {
+    return Math.round((a + b) * Math.pow(10, roundedDecimals)) / Math.pow(10, roundedDecimals)
+};
+function subtractFunc(a, b) {
+    return Math.round((a - b) * Math.pow(10, roundedDecimals)) / Math.pow(10, roundedDecimals)
+};
+function multiplyFunc(a, b) {
+    return Math.round((a * b) * Math.pow(10, roundedDecimals)) / Math.pow(10, roundedDecimals)
+};
 function divideFunc(a, b) { 
     if (b === 0) {
-        return "ERROR"
-    } else {
-    return a / b }};
+        clearValues();
+        return "ERROR";
+    }
+    return Math.round((a / b) * Math.pow(10, roundedDecimals)) / Math.pow(10, roundedDecimals)
+};
 // #endregion
 
 // #region --- system funcs
@@ -49,26 +60,70 @@ function clearValues() {
     secondValue = null;
     operator = null;
     result = null;
+    userInput = [];
+    displayResult.textContent = null;
+};
+function displayVariables() {
+    console.log("-----");
+    console.log(`firstValue is ${firstValue}`);
+    console.log(`operator is ${operator}`);
+    console.log(`secondValue is ${secondValue}`);
+    console.log(`result is ${result}`);
+    console.log(`userInput is ${userInput}`);
+    console.log("-----");
+};
+function deleteLast() { // check again later
+    deleteBtn.addEventListener("click", () => {
+        userInput.pop();
+        displayResult.textContent = userInput.join("");
+    })
+    displayVariables();
+};
+function calcValue(firstValue, secondValue, operator) {
+    operateFunc(firstValue, secondValue, operator);
+    displayResult.textContent = result;
+    userInput.push(result);
+    userInput = [userInput[userInput.length -1]];
+    firstValue = result;
+    secondValue = null;
+    displayVariables();
 };
 // #endregion
 
 // #region --- input processing
-function processInput(input) {
-    if (isDigit(input)) {
-        processDigit(input);
-    } else if (isDecimal(input)) {
-        processDecimal(input);
-    } else if (isOperator(input)) {
-        processOperator(input);
+function processInput(userInput) {
+    for (let input of userInput) {
+        if (isNumber(input)) {
+            processNumber(input);
+        } else if (isDecimal(input)) {
+            processDecimal(input);
+        } else if (isOperator(input)) {
+            processOperator(input);
+        }
     }
+    displayVariables();
 };
 
-function isDigit(input) { return "0123456789".includes(input); };
-function isDecimal(input) { return input === "."; };
-function isMinus(input) { return input === "-"; };
-function isOperator(input) { return "+-*/".includes(input); };
+function isNumber(input) {
+    return "0123456789".includes(input);
+};
+function isDecimal(input) {
+    return input === ".";
+};
+function isMinus(input) {
+    return input === "-";
+};
+function isOperator(input) {
+    return "+-*/".includes(input);
+};
 
-function processDigit(input) {
+function processNumber(input) {
+    if (calculated && operator === null) {
+        clearValues()
+        userInput.push(input)
+        displayResult.textContent = userInput.join("");
+    };
+    calculated = false;
     if (firstValue === null && operator === null && secondValue === null) {
         firstValue = input;
     } else if (firstValue != null && operator === null && secondValue === null) {
@@ -78,193 +133,132 @@ function processDigit(input) {
     } else if (firstValue != null && operator != null && secondValue != null) {
         secondValue += input;
     }
+    displayVariables();
 };
-
 function processDecimal(input) {
-    if (firstValue != null && operator === null && secondValue === null) {
+    if (firstValue === null) {
+        firstValue = `0${input}`
+    } else if (firstValue != null && operator === null && secondValue === null) {
         if (!firstValue.includes(".")) {
             firstValue += input;
         }
+    } else if (firstValue != null && operator != null && secondValue === null) {
+        secondValue = `0${input}`
     } else if (firstValue != null && operator != null && secondValue != null) {
         if (!secondValue.includes(".")) {
             secondValue += input;
         }
     }
 };
-
 function processOperator(input) {
-    if (isMinus(input) && firstValue === null && operator === null && secondValue === null) {
-        firstValue = input;
-    } else if (operator === null) {
+    if (firstValue != null && operator != null & secondValue != null) {
+        calcValue(firstValue, secondValue, operator);
+        firstValue = result;
+        secondValue = null;
         operator = input;
+        userInput.push(input);
+        displayResult.textContent = userInput.join("");
+    // } else if (firstValue != null && operator != null & secondValue === null) {
+
+    } else if (isMinus(input)) {
+        if (firstValue === null) {
+            firstValue = input;
+        } else if (firstValue != null && operator === null) { // may not need this, drops to elseif block?
+            operator = input;
+        } else if (secondValue === null) {
+            secondValue = input;
+        }
+    } else if (firstValue != null && operator === null) {
+        operator = input;
+        calculated = false;
     }
 };
 // #endregion
 
+// #region --- display functions
+function displayNumber() {
+    numberBtn.forEach(number => {
+        number.addEventListener("click", () => {
+            input = number.textContent;
+            userInput.push(input);
+            displayResult.textContent = userInput.join("");
 
+            processInput(input);
+            console.log(`Last input: ${input}`);
+        })
+    })
+};
 
-// #region --- old number and function inputs
-// const buttonContainer = document.querySelector(".button-container");
-// const buttons = document.querySelector(".buttons");
+function displayOperator() {
+    operatorBtn.forEach(operate => {
+        operate.addEventListener("click", () => {
+            input = operate.textContent;
 
-// // number buttons
-// const numbers = document.querySelectorAll(".number");
-// const one = document.getElementById("one");
-// const two = document.getElementById("two");
-// const three = document.getElementById("three");
-// const four = document.getElementById("four");
-// const five = document.getElementById("five");
-// const six = document.getElementById("six");
-// const seven = document.getElementById("seven");
-// const eight = document.getElementById("eight");
-// const nine = document.getElementById("nine");
-// const zero = document.getElementById("zero");
+            if (operator === null) {
+                userInput.push(input);
+                displayResult.textContent = userInput.join("");
+                processInput(input);
+                console.log(`Last input: ${input}`);
+            // checks last char is operator and second to last char is not operator
+            } else if (isOperator(userInput[userInput.length - 1]) && !isOperator(userInput[userInput.length - 2])) {
+                // if input is not minus, pops last char operator off userInput[]
+                if (input === "-") {
+                } else if ("+*/".includes(input)) {
+                    userInput.pop();
+                }
+                // adds input to userInput[]
+                userInput.push(input);
+                // displays userInput values
+                displayResult.textContent = userInput.join("");
+                processInput(input);
+                console.log(`Last input: ${input}`);
+            } else if (isMinus(input) && isOperator(userInput[userInput.length - 2])) {
+                userInput.push(input);
+                displayResult.textContent = userInput.join("");
+                processInput(input);
+                console.log(`Last input: ${input}`);
+            }
+        })
+    })
+};
 
-// // function buttons
-// const operators = document.querySelector(".operator");
-// const enter = document.getElementById("enter");
-// const add = document.getElementById("add");
-// const subtract = document.getElementById("subtract");
-// const multiply = document.getElementById("multiply");
-// const divide = document.getElementById("divide");
-// const clear = document.getElementById("clear");
-// // const parentheses = document.getElementById("parentheses");
-// // const delete = document.getElementById("delete");
-// // const parentheses = document.getElementById("parentheses");
-// // const decimal = document.getElementById("decimal");
+function displayDecimal() {
+    decimalBtn.addEventListener("click", () => {
+        input = decimalBtn.textContent;
+        if (isDecimal(userInput[userInput.length - 1])) {
+            userInput.pop();
+        }
+        userInput.push(input);
+        displayResult.textContent = userInput.join("");
 
-// buttons.addEventListener("click", (event) => {
-//     let target = event.target;
-    
-//     switch (target.id) {
-//         case "one":
-//             if (a === undefined) {
-//                 a = "1";
-//             } else {
-//                 a += "1";
-//             };
-//             display.textContent += "1";
-//             break;
-//         case "two":
-//             if (a === undefined) {
-//                 a = "2";
-//             } else {
-//                 a += "2";
-//             };            
-//             display.textContent += "2";
-//             break;
-//         case "three":
-//             if (a === undefined) {
-//                 a = "3";
-//             } else {
-//                 a += "3";
-//             };            
-//             display.textContent += "3";
-//             break;
-//         case "four":
-//             if (a === undefined) {
-//                 a = "4";
-//             } else {
-//                 a += "4";
-//             };            
-//             display.textContent += "4";
-//             break;
-//         case "five":
-//             if (a === undefined) {
-//                 a = "5";
-//             } else {
-//                 a += "5";
-//             };            
-//             display.textContent += "5";
-//             break;
-//         case "six":
-//             if (a === undefined) {
-//                 a = "6";
-//             } else {
-//                 a += "6";
-//             };            
-//             display.textContent += "6";
-//             break;
-//         case "seven":
-//             if (a === undefined) {
-//                 a = "7";
-//             } else {
-//                 a += "7";
-//             };            
-//             display.textContent += "7";
-//             break;
-//         case "eight":
-//             if (a === undefined) {
-//                 a = "8";
-//             } else {
-//                 a += "8";
-//             };            
-//             display.textContent += "8";
-//             break;
-//         case "nine":
-//             if (a === undefined) {
-//                 a = "9";
-//             } else {
-//                 a += "9";
-//             };            
-//             display.textContent += "9";
-//             break;
-//         case "zero":
-//             if (a === undefined) {
-//                 a = "0";
-//             } else {
-//                 a += "0";
-//             };           
-//             display.textContent += "0";
-//             break;
-//         case "add":
-//             if(a != "") {
-//                 operator = "+";
-//                 display.textContent += " + ";
-//                 b = a;
-//                 a = "";
-//             };
-//             break;
-//         case "subtract":
-//             if(a != "") {
-//                 operator = "-";
-//                 display.textContent += " - ";
-//                 b = a;
-//                 a = "";
-//             };
-//             break;
-//         case "multiply":
-//             if(a != "") {
-//                 operator = "*";
-//                 display.textContent += " * ";
-//                 b = a;
-//                 a = "";
-//             };
-//             break;
-//         case "divide":
-//             if(a != "") {
-//                 operator = "/";
-//                 display.textContent += " / ";
-//                 b = a;
-//                 a = "";
-//                 // console.log(a);
-//                 // console.log(operator);
-//                 // console.log(b);
-//             };
-//             break;
-//         case "enter":
-//             if (a != "" && b != "" && operator != "") {
-//                 result = operateFunc(a, b, operator);
-//                 display.textContent = result;
-//                 a = result;
-//             };
-//             break;
-//         case "clear":
-//             clearValues();
-//             break;
-//     }
-//     console.log(`"${a}" is stored in a`);
-//     console.log(`"${operator}" is stored in operator`);
-//     console.log(`"${b}" is stored in b`);
-// });
+        processInput(input);
+        console.log(`Last input: ${input}`);
+    })
+};
 // #endregion
+
+// equal button
+equalBtn.addEventListener("click", () => {
+    calcValue(firstValue, secondValue, operator);
+    firstValue = result;
+    secondValue = null;
+    operator = null;
+    calculated = true;
+    displayVariables();
+});
+
+// clear button
+clearBtn.addEventListener("click", () => {
+    clearValues();
+    displayVariables();
+});
+
+// main function
+function main() {
+    displayNumber();
+    displayOperator();
+    displayDecimal();
+    deleteLast();
+};
+
+main();
